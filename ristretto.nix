b6a6717621c978e0ae5524604dc3e37fecd16dde
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-07-16 13:59:48 -0400

    Get ristretto to build

--- ristretto.nix
+++ ristretto.nix
@@ -5,8 +5,10 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "brave-intl";
     repo = "challenge-bypass-ristretto-ffi";
-    rev = "${version}";
-    sha256 = "06lxs4rxy4i9l4wjm1wqi4dnqscpzgp7v24ayp524gwgf3yc1sd9";
+    # Pick the most recent version I can actually get to build.
+    # https://github.com/brave-intl/challenge-bypass-ristretto-ffi/issues/48
+    rev = "a4b1ac1262920e41833425908e59d7ebadb31e19";
+    sha256 = "0bwph820gnha6hhhgb9zb8qxlp1958b9rikwm4480mnij7mmb778";
   };
   cargoSha256 = "1qbfp24d21wg13sgzccwn3ndvrzbydg0janxp7mzkjm4a83v0qij";
 }
