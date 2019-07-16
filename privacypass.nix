983397a7e95536498d6404ef3a95b7625f1778af
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-07-16 15:19:35 -0400

    Fix the privacypass build

--- privacypass.nix
+++ privacypass.nix
@@ -1,17 +1,30 @@
-{ ristretto, git, python, pythonPackages, setuptools_scm, milksnake }:
+{ ristretto, git, python, pythonPackages, setuptools_scm, milksnake, cffi }:
 pythonPackages.buildPythonPackage rec {
   version = "0.0.0";
   pname = "privacypass";
   name = "${pname}-${version}";
   src = ./.;
 
-  nativeBuildInputs = [
+  postUnpack = ''
+  substituteInPlace $sourceRoot/setup.py \
+      --replace "['cargo', 'build', '--release']" "['sh', '-c', ':']" \
+      --replace "./challenge-bypass-ristretto-ffi" "/" \
+      --replace "target/release" "${ristretto}/lib" \
+      --replace "./src" "${src}/challenge-bypass-ristretto-ffi/src"
+  '';
+
+  propagatedNativeBuildInputs = [
     git
     ristretto
+  ];
+
+  propagatedBuildInputs = [
+    cffi
   ];
 
   buildInputs = [
     setuptools_scm
     milksnake
   ];
+
 }
