cdd5944d98513dfb293e6c2b9dd8d0a782d3acb2
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-07-18 12:38:53 -0400

    some comments

--- default.nix
+++ default.nix
@@ -1,7 +1,13 @@
+# A basic Python 2.7-based packaging of the Python bindings for the
+# Ristretto-based PrivacyPass variant.
 { pkgs ? import <nixpkgs> { } }:
 let
-ristretto = pkgs.callPackage ./ristretto.nix { };
+  # Brave's Ristretto library is not in nixpkgs so provide the package we're
+  # maintaining.
+  ristretto = pkgs.callPackage ./ristretto.nix { };
 in
+  # Build our Python bindings in the usual way, supplying the necessary extra
+  # dependency.
   pkgs.python27Packages.callPackage ./privacypass.nix {
     inherit ristretto;
   }
