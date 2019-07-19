11e149cdad11947d93c8293d798e82efa9f53420
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-07-19 19:47:33 -0400

    Make a convenient way to have Hypothesis really bang on it

---
+++ privacypass/tests/__init__.py
@@ -0,0 +1,16 @@
+def _configure_hypothesis():
+    from os import environ
+
+    from hypothesis import (
+        HealthCheck,
+        settings,
+    )
+
+    settings.register_profile(
+        "long",
+        max_examples=100000,
+    )
+
+    profile_name = environ.get("PRIVACYPASS_HYPOTHESIS_PROFILE", "default")
+    settings.load_profile(profile_name)
+_configure_hypothesis()
