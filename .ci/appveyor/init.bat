3f9509518e632506959ea200f10caa924ca29cb9
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-09-30 11:51:46 -0400

    Perhaps get cargo on %PATH% or whatever

--- .ci/appveyor/init.bat
+++ .ci/appveyor/init.bat
@@ -1,6 +1,9 @@
 Rem Get a Rust build toolchain.
 choco install rustup.install --version 1.19.0
 
+Rem Make it possible to run the "cargo" command
+refreshenv
+
 Rem Build challenge-bypass-ristretto-ffi.
 cd challenge-bypass-ristretto-ffi
 cargo build --release
