2930c25e6cad592ff4f5f6c43d46eb488f4f0dac
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-09-30 11:49:30 -0400

    skip the Makefile, which does a bunch of stuff and requires make
    Just run the cargo build command

--- .ci/appveyor/init.bat
+++ .ci/appveyor/init.bat
@@ -3,7 +3,7 @@ choco install rustup.install --version 1.19.0
 
 Rem Build challenge-bypass-ristretto-ffi.
 cd challenge-bypass-ristretto-ffi
-make
+cargo build --release
 
 Rem Install it so we can link the Python extension against it.
 make install
