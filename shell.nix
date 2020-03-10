1f22e38eaf6b49bcbf70cfceab19c97b7c5371bd
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-03-10 19:58:36 -0400

    duplicate the `rootCrate.build` fix for the crate2nix expression

--- shell.nix
+++ shell.nix
@@ -1,10 +1,10 @@
 { pkgs ? import <nixpkgs> { } }:
 let
-pythonPackages = pkgs.python27Packages;
-python = pkgs.python27;
-ristretto = pkgs.callPackage ./ristretto.nix { };
-challenge-bypass-ristretto = pythonPackages.callPackage ./python-challenge-bypass-ristretto.nix {
-  inherit ristretto;
-};
+  pythonPackages = pkgs.python27Packages;
+  python = pkgs.python27;
+  ristretto = (pkgs.callPackage ./ristretto.nix { }).rootCrate.build;
+  challenge-bypass-ristretto = pythonPackages.callPackage ./python-challenge-bypass-ristretto.nix {
+    inherit ristretto;
+  };
 in
-(python.withPackages (ps: [ challenge-bypass-ristretto ])).env
+  (python.withPackages (ps: [ challenge-bypass-ristretto ])).env
