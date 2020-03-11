0ac2df22bffc8757b9a375a72af05ee97163745c
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-03-11 09:33:47 -0400

    Factor out the Darwin-only Security-adding override

--- challenge-bypass-ristretto.nix
+++ challenge-bypass-ristretto.nix
@@ -1,9 +1,10 @@
 { pkgs, stdenv, lib, darwin }:
 let
+  withSecurity = attrs: {
+    buildInputs = lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Security;
+  };
   defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-    curve25519-dalek = attrs: {
-      buildInputs = lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Security;
-    };
+    curve25519-dalek = withSecurity;
   };
   challenge-bypass-ristretto = pkgs.callPackage ./generated-challenge-bypass-ristretto.nix {
     inherit defaultCrateOverrides;
