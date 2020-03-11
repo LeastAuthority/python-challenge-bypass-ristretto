282b54a35a9d33b26c77e703ec6ddc47eec1970d
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-03-11 09:06:58 -0400

    Try with defaultCrateOverrides

--- challenge-bypass-ristretto.nix
+++ challenge-bypass-ristretto.nix
@@ -1,19 +1,12 @@
-{ pkgs, stdenv, darwin }:
+{ pkgs, stdenv, lib, darwin }:
 let
-  challenge-bypass-ristretto = pkgs.callPackage ./generated-challenge-bypass-ristretto.nix { };
-  addSecurity = drv:
-    if stdenv.isDarwin then
-      drv.overrideAttrs (old: {
-        nativeBuildInputs = [
-          darwin.apple_sdk.frameworks.Security
-        ];
-      })
-    else
-      drv;
-in
-  challenge-bypass-ristretto // {
-    rootCrate = {
-      build = addSecurity challenge-bypass-ristretto.rootCrate.build;
-      debug = addSecurity challenge-bypass-ristretto.rootCrate.debug;
+  defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+    curve25519-dalek = attrs: {
+      nativeBuildInputs = lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.Security;
     };
-  }
+  };
+  challenge-bypass-ristretto = pkgs.callPackage ./generated-challenge-bypass-ristretto.nix {
+    inherit defaultCrateOverrides;
+  };
+in
+  challenge-bypass-ristretto
