324541d40e099c5f5bad24cd13e1c4b4d0accef3
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-03-13 12:34:34 -0400

    See if we can get cargo on PATH

--- .github/workflows/manylinux2010-install-rust.py
+++ .github/workflows/manylinux2010-install-rust.py
@@ -4,3 +4,6 @@ curl --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup-init
 sh /tmp/rustup-init -y --default-host x86_64-unknown-linux-gnu --default-toolchain stable
 rm /tmp/rustup-init
 """]))
+
+import os
+os.environ["PATH"] += ":/github/home/.cargo/bin"
