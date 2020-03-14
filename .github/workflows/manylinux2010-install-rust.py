0d1f97506c064a282d095ca4a86c82631edc6a1f
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-03-14 15:51:43 -0400

    Try to get the right version for manylinux2010

--- .github/workflows/manylinux2010-install-rust.py
+++ .github/workflows/manylinux2010-install-rust.py
@@ -3,6 +3,10 @@ print(subprocess.check_output(["/bin/bash", "-c", """
 curl --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup-init
 sh /tmp/rustup-init -y --default-host x86_64-unknown-linux-gnu --default-toolchain stable
 rm /tmp/rustup-init
+
+# The change to run this script patched into setup.py changes the project
+# version by making the working copy dirty.  Clean the working copy.
+git reset --hard
 """]))
 
 import os
