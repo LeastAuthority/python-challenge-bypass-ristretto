484d4b4ee3cc26bc1556a5cc478c0d10d39eabb7
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-03-17 09:08:16 -0400

    maybe doing what the installer says to do will work better
    I doubt it

--- .github/workflows/install-rust.sh
+++ .github/workflows/install-rust.sh
@@ -4,4 +4,4 @@ curl --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup-init
 sh /tmp/rustup-init -y --default-host x86_64-unknown-linux-gnu --default-toolchain stable
 rm /tmp/rustup-init
 
-export PATH="$PATH":/github/home/.cargo/bin
+. "$HOME"/.cargo/env
