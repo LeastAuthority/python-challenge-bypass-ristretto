5d88907a9ca57664f7a60834e44e3851d6ea3069
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-03-17 09:10:24 -0400

    It is not a coherent stand-alone script.

--- .github/workflows/install-rust.sh
+++ .github/workflows/install-rust.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+# Source this so I can fix your PATH and stuff.
 
 curl --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup-init
 sh /tmp/rustup-init -y --default-host x86_64-unknown-linux-gnu --default-toolchain stable
