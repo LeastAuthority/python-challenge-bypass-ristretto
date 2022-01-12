6ffaf700c8540195031db98c3279b3f66f74791c
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-01-12 14:30:51 -0500

    adapt the CI test runner to the new shape of things

--- ci-tools/run-tests.sh
+++ ci-tools/run-tests.sh
@@ -2,11 +2,16 @@
 
 set -euxo pipefail
 
-nix-build --arg pkgs 'import <nixpkgs> {}' challenge-bypass-ristretto.nix -A tests
+# Run the ffi binding tests
+nix-build -A tests --out-link ffi-tests challenge-bypass-ristretto.nix
 
-# Run what passes for the test suite for our Python code, too.  Ditto here
-# about checkPhase.
-if ! nix-shell shell.nix --run 'cp ./spike.py /tmp; python /tmp/spike.py'; then
+# Build the Python package itself
+nix-build --out-link result
+
+# Run what passes for the test suite for our Python code, too.  It would be
+# nice to put this into a tests attribute on the Python package derivation,
+# probably.
+if ! nix-shell -p ./result --run 'cp ./spike.py /tmp; python /tmp/spike.py'; then
     echo "spike failed"
     exit 2
 fi
