33de06750439c938d91e284cc1b4f1c5573d1242
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-12-07 11:39:45 -0500

    some changes, I dunno, gonna delete this

--- naersk/flake.nix
+++ naersk/flake.nix
@@ -6,22 +6,29 @@
       url = "github:brave-intl/challenge-bypass-ristretto-ffi?rev=f88d942ddfaf61a4a6703355a77c4ef71bc95c35";
       flake = false;
     };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-22.05";
   };
 
   outputs = { self, flake-utils, naersk, nixpkgs, challenge-bypass-ristretto-ffi-src }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = (import nixpkgs) {
+        native-pkgs = import nixpkgs {
           inherit system;
         };
+        cross-pkgs = import nixpkgs {
+          inherit system;
+          crossSystem = (import nixpkgs { inherit system; }).lib.systems.examples.aarch64-android-prebuilt;
+        };
 
-        naersk' = pkgs.callPackage naersk {};
+        naersk' = cross-pkgs.callPackage naersk {};
 
       in rec {
+
         # For `nix build` & `nix run`:
-        defaultPackage = naersk'.buildPackage {
+        packages.default = naersk'.buildPackage {
           src = challenge-bypass-ristretto-ffi-src;
+          rustc = "This is it!!!!";
+          copyTarget = true;
         };
       }
     );
