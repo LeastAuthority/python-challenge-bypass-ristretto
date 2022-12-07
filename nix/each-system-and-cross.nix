d42cd4faadca354e5fe49339aa0f374238a6c951
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2022-12-07 16:22:06 -0500

    Some docs for this function

--- nix/each-system-and-cross.nix
+++ nix/each-system-and-cross.nix
@@ -1,4 +1,54 @@
+# Apply a derivation building function to the product of some build and host
+# systems to help with the definition of cross-compiled versions of the
+# derivation.
+#
+# Consider the type aliases::
+#
+#   System = string
+#     The name of a system (as in "x86_64-linux")
+#
+#   Derivation = attrset
+#     A nix derivation value
+#
+#   PackageName = string
+#     The name of a package in a package set
+#
+#   PackageSet = AttrSet PackageName Derivation
+#
+#     A collection of named packages.
+#
+#   CrossPackageSet = AttrSet
+#     { packages  :: PackageSet
+#     , pkgsCross :: PackageSet
+#     , devShells :: PackageSet
+#     }
+#
+# and a builder function::
+#
+#   f :: System -> System -> CrossPackageSet
+#
+# this call::
+#
+#   eachSystemAndCross {
+#     inherit (pkgs) lib;
+#     buildSystems = [ "x86_64-linux" ];
+#     crossSystems = [ "i686-linux" "aarch64-linux" "riscv-linux" ];
+#   } f
+#
+# will return a value which can be returned from a flake `outputs` function
+# which defines cross-compiled packages for i686-linux, aarch64-linux, and
+# riscv-linux for an x86_64-linux build system.
+#
+# Derivations in the `packages` attribute in the return value of `f` will be
+# present as normal, native-compilation `packages` in the output.
+#
+# Derivations in the `pkgsCross` attribute in the return value of `f` will be
+# exposed via `legacyPackages` beneath `pkgsCross.${crossSystem}`.
+#
+# Derivations in the `devShells` attribute in the return value of `f` will be
+# present as normal, native-compilation `devShells` in the output.
 {
+# the nixpkgs library
   lib
 
 # A list of nixpkgs system identifiers as strings (tuples, triples, maybe
